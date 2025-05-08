7cb0145aa4f818039256b8240314c55de1fb0ec2
jfeito26 <jorgefeicar26@gmail.com>
2025-05-08 19:05:36 +0200

    APARTADO5

--- Assets/Scripts/FlyingEnemy.cs
+++ Assets/Scripts/FlyingEnemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FlyingEnemy : MonoBehaviour
 {
+    private float currentTime = 0, Xspeed = 1f, Yspeed = 1f;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,13 @@ public class FlyingEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       transform.Translate(new Vector2(Xspeed, Mathf.Sin(Time.time) * Yspeed) * Time.deltaTime);
+       if(currentTime == 5 )
+        {
+            //transform.Translate(new Vector2(Xspeed, Mathf.Sin(Time.time) * (-Xspeed)) * Time.deltaTime);
+            //transform.Translate(Vector2(Xspeed * (-Xspeed) * Yspeed) *Time.deltaTime);
+            //Xspeed = -1f;
+            //Xspeed = -Xspeed;
+        } 
     }
 }
